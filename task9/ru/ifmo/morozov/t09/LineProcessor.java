233db0cfa895e741d65fbec2267971236080fb24
vks <vks_m@mail.ru>
2015-04-18 13:40:22 +0000

    Rewrote matcher & added packages

--- task9/ru/ifmo/morozov/t09/LineProcessor.java
+++ task9/ru/ifmo/morozov/t09/LineProcessor.java
@@ -1,10 +1,10 @@
+package ru.ifmo.morozov.t09;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -47,7 +47,8 @@ public class LineProcessor {
                     }
                     case "filter": {
                         String regex = args[i + 1];
-                        stream = findLines(stream.collect(Collectors.toList()), regex);
+                        Pattern pattern = Pattern.compile(regex);
+                        stream = stream.filter(s -> pattern.matcher(s).find());
                         break;
                     }
                 }
@@ -58,18 +59,4 @@ public class LineProcessor {
         }
     }
 
-    private static Stream<String> findLines(List<String> list, String regex) {
-        List<String> strings = new ArrayList<>();
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher;
-
-        for (int i = 0; i < list.size(); i++) {
-            matcher = pattern.matcher(list.get(i));
-            if (matcher.find()) {
-                strings.add(list.get(i));
-            }
-        }
-
-        return strings.stream();
-    }
 }
